d077ff89946dcd744e2da5e0614fc3dffe157d6e
Steven D'Aprano <steve+python@pearwood.info>
2015-01-05 05:01:16 +1100

    Update setup file.

--- setup.py
+++ setup.py
@@ -22,46 +22,49 @@ except LookupError:
 setup(
     name = "pyprimes",
     package_dir={'': 'src'},
-    py_modules=["pyprimes"],
+    packages = ['pyprimes'],
     version = __version__,
     author = __author__,
     author_email = __author_email__,
     url = 'http://code.google.com/p/pyprimes/',
-    keywords = "prime primes math maths algorithm fermat miller-rabin".split(),
+    keywords = "prime primes primality math maths algorithm fermat miller-rabin".split(),
     description = "Generate and test for prime numbers.",
     long_description = """\
-Compare a variety of algorithms for generating and testing prime numbers
-with the pure-Python module ``pyprimes``.
+The pyprimes package offers a variety of algorithms for generating prime
+numbers and fast primality test, written in pure Python.
 
 Prime numbers are those positive integers which are not divisible exactly
 by any number other than itself or one. Generating primes and testing for
 primality has been a favourite mathematical pastime for centuries, as well
 as of great practical importance for encrypting data.
 
-Features of ``pyprimes``:
+``pyprimes`` includes the following features:
 
     - Produce prime numbers lazily, on demand.
-    - Effective, fast algorithms including Sieve of Eratosthenes,
-      Croft Spiral, and Wheel Factorisation.
-    - Test whether numbers are prime efficiently.
-    - Deterministic and probabilistic primality tests.
-    - Examples of what *not* to do provided, including naive trial
-      division, Turner's algorithm, and primality testing using a 
+    - Effective algorithms including Sieve of Eratosthenes, Croft Spiral,
+      and Wheel Factorisation.
+    - Efficiently test whether numbers are prime, using both deterministic
+      (exact) and probabilistic primality tests.
+    - Examples of what *not* to do are provided, including naive trial
+      division, Turner's algorithm, and primality testing using a
       regular expression.
-    - Factorise numbers into the product of prime factors.
-    - Suitable for Python 2.5 through 3.2 from one code base.
+    - Factorise small numbers into the product of prime factors.
+    - Suitable for Python 2.4 through 3.2 from one code base.
 
 """,
     license = 'MIT',  # apologies for the American spelling
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.4",
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.0",
         "Programming Language :: Python :: 3.1",
         "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
